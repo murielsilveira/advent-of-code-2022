6e34b29a392f0727fe63361e18fc60c49aad5111
Muriel Silveira <murielkong@gmail.com>
2022-12-04 18:30:42 -0300

    feat(rust): solve day01.p1

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("day01");
+    let content = fs::read_to_string("../../_inputs/day01.txt").unwrap();
+
+    let max_calories = content
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            let calories: u32 = elf
+                .split("\n")
+                .map(|cal_str| cal_str.parse::<u32>().unwrap())
+                .sum();
+
+            calories
+        })
+        .max()
+        .unwrap();
+
+    println!("{}", max_calories);
 }
