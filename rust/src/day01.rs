18e6f3563d6e36a249b1f396553ff704b1749985
Muriel Silveira <murielkong@gmail.com>
2022-12-10 15:41:16 -0300

    chore(rust): organize project structure

---
+++ rust/src/day01.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+pub fn solve() {
+    let content = fs::read_to_string("../_inputs/day01.txt").unwrap();
+
+    let mut elves = content
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            let calories: u32 = elf
+                .split('\n')
+                .map(|cal_str| cal_str.parse::<u32>().unwrap())
+                .sum();
+
+            calories
+        })
+        .collect::<Vec<_>>();
+
+    elves.sort();
+
+    let top_one_calories = &elves[elves.len() - 1..].iter().sum::<u32>();
+    let top_three_calories_sum = &elves[elves.len() - 3..].iter().sum::<u32>();
+
+    println!("day01.p1 {:?}", top_one_calories);
+    println!("day01.p2 {:?}", top_three_calories_sum);
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,5 @@
-use std::fs;
+mod day01;
 
 fn main() {
-    let content = fs::read_to_string("../_inputs/day01.txt").unwrap();
-
-    let mut elves = content
-        .trim()
-        .split("\n\n")
-        .map(|elf| {
-            let calories: u32 = elf
-                .split("\n")
-                .map(|cal_str| cal_str.parse::<u32>().unwrap())
-                .sum();
-
-            calories
-        })
-        .collect::<Vec<_>>();
-
-    elves.sort();
-
-    let top_one_calories = &elves[elves.len() - 1..].iter().sum::<u32>();
-    let top_three_calories_sum = &elves[elves.len() - 3..].iter().sum::<u32>();
-
-    println!("day01_1 {:?}", top_one_calories);
-    println!("day01_2 {:?}", top_three_calories_sum);
+    day01::solve();
 }
